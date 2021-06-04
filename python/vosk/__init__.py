eeb575895ceded50db4f7c890cdab569ebe2c1a2
Igor Sitdikov <igor.sitdikov@codex-soft.com>
2021-06-04 18:35:39 +0300

    fix;

--- python/vosk/__init__.py
+++ python/vosk/__init__.py
@@ -20,17 +20,6 @@ def open_dll():
 
 _c = open_dll()
 
-class Model(object):
-
-    def __init__(self, model_path):
-        self._handle = _c.vosk_model_new(model_path.encode('utf-8'))
-
-    def __del__(self):
-        _c.vosk_model_free(self._handle)
-
-    def vosk_model_find_word(self, word):
-        return _c.vosk_model_find_word(self._handle, word.encode('utf-8'))
-
 class SpkModel(object):
 
     def __init__(self, model_path):
@@ -43,19 +32,12 @@ class KaldiRecognizer(object):
 
     def __init__(self, *args):
         if len(args) == 2:
-            self._handle = _c.vosk_recognizer_new(args[0]._handle, args[1])
-        elif len(args) == 3 and type(args[2]) is SpkModel:
-            self._handle = _c.vosk_recognizer_new_spk(args[0]._handle, args[1], args[2]._handle)
-        elif len(args) == 3 and type(args[2]) is str:
-            self._handle = _c.vosk_recognizer_new_grm(args[0]._handle, args[1], args[2].encode('utf-8'))
+            self._handle = _c.vosk_recognizer_new_spk(args[0]._handle, args[1])
         else:
             raise TypeError("Unknown arguments")
 
     def __del__(self):
         _c.vosk_recognizer_free(self._handle)
-
-    def SetMaxAlternatives(self, max_alternatives):
-        _c.vosk_recognizer_set_max_alternatives(self._handle, max_alternatives)
 
     def SetSpkModel(self, spk_model):
         _c.vosk_recognizer_set_spk_model(self._handle, spk_model._handle)
@@ -63,26 +45,10 @@ class KaldiRecognizer(object):
     def AcceptWaveform(self, data):
         return _c.vosk_recognizer_accept_waveform(self._handle, data, len(data))
 
-    def Result(self):
-        return _ffi.string(_c.vosk_recognizer_result(self._handle)).decode('utf-8')
-
-    def PartialResult(self):
-        return _ffi.string(_c.vosk_recognizer_partial_result(self._handle)).decode('utf-8')
-
     def FinalResult(self):
         return _ffi.string(_c.vosk_recognizer_final_result(self._handle)).decode('utf-8')
-
-    def Reset(self):
-        return _c.vosk_recognizer_reset(self._handle)
 
 
 def SetLogLevel(level):
     return _c.vosk_set_log_level(level)
 
-
-def GpuInit():
-    _c.vosk_gpu_init()
-
-
-def GpuThreadInit():
-    _c.vosk_gpu_thread_init()
